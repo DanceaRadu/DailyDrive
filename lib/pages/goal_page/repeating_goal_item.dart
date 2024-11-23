0a1561b788a2aca267ec2871f20502973f482726
DanceaRadu <radu.dancea@gmail.com>
2024-11-24 00:31:02 +0200

    Finished working on the repeating_goal_details_page.dart

--- lib/pages/goal_page/repeating_goal_item.dart
+++ lib/pages/goal_page/repeating_goal_item.dart
@@ -3,6 +3,8 @@ import 'package:daily_drive/models/goal_period.model.dart';
 import 'package:daily_drive/models/repeating_goal.model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class RepeatingGoalItem extends StatelessWidget {
 
@@ -38,33 +40,56 @@ class RepeatingGoalItem extends StatelessWidget {
     }
 
     String percentageString = '${(goalPeriod.progress / goal.goal * 100).toStringAsFixed(0)}%';
+    String formattedDate = DateFormat('d MMM y').format(goalPeriod.periodStart);
 
     return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 0.0),
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 0.0),
         child: Row(
           children: [
-            Icon(
-              icon,
-              color: iconColor,
-              size: 45.0,
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(
+                icon,
+                color: iconColor,
+                size: 50.0,
+              ),
             ),
             Expanded(
-              child: const Column(
+              child: Column(
                 children: [
-                  Text("asdasdasd"),
-                  Text("asdasdasdasd")
+                  SizedBox(
+                      width: double.infinity,
+                      child: Text(formattedDate)
+                  ),
+                  const SizedBox(height: 5),
+                  LinearPercentIndicator(
+                    lineHeight: 20.0,
+                    percent: goalPeriod.progress / goal.goal,
+                    backgroundColor: ColorPalette.darkerSurface,
+                    progressColor: iconColor,
+                    animation: true,
+                    barRadius: const Radius.circular(8.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 0.0),
+                  )
                 ],
               ),
             ),
             Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  percentageString,
-                  style: const TextStyle(
-                    color: ColorPalette.textColor,
-                    fontSize: 16.0,
+                SizedBox(
+                  width: 50.0,
+                  height: 45,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Text(
+                      percentageString,
+                      style: const TextStyle(
+                        color: ColorPalette.textColor,
+                        fontSize: 15.0,
+                      ),
+                    ),
                   ),
                 ),
               ],
